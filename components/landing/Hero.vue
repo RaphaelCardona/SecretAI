b5628222cdc14272bd26808e4c311af2528b6ef8
Raphael Cardona <creativerapha@gmail.com>
2024-12-08 21:34:48 -0500

    Update Hero.vue

--- components/landing/Hero.vue
+++ components/landing/Hero.vue
@@ -3,7 +3,7 @@
     <div class="p-24 md:order-1 hidden md:block">
       <img
         class="rounded-full transition-transform transform hover:scale-125"
-        src="https://ibb.co/RYYvCc1"
+        src="~/assets/img/hero.jpg"
         alt="Secret AI"
         loading="eager"
         format="avif"
