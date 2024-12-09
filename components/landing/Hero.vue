3cb8c633fb2286a5235137bbe3697ec32e0bf8f8
Raphael Cardona <creativerapha@gmail.com>
2024-12-08 21:31:53 -0500

    Update Hero.vue

--- components/landing/Hero.vue
+++ components/landing/Hero.vue
@@ -24,15 +24,15 @@
           href="https://secret.kairoscrypto.org"
           target="_blank"
           rel="noopener"
-          >Try out our AI Beta</LandingLink
+          >Try out our AI (Beta)</LandingLink
         >
         <LandingLink
           size="lg"
           styleName="outline"
           rel="noopener"
-          href="https://github.com/Gr33nW33n/nuxtship-template"
+          href="https://docs.kairoscrypto.org"
           target="_blank"
-          >View Repo</LandingLink
+          >View Docs</LandingLink
         >
       </div>
     </div>
